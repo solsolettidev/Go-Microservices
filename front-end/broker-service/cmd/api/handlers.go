633c6a0093b7fa3fc5a1fb3c1acdaf9c8380ecd5
Sol Soletti <solettisool@gmail.com>
2026-04-20 20:27:30 -0300

    added a route and handler to comunicate with logger service

--- front-end/broker-service/cmd/api/handlers.go
+++ front-end/broker-service/cmd/api/handlers.go
@@ -8,13 +8,19 @@ import (
 )
 
 type RequestPayload struct{
-	Action string `json:"action"`
-	Auth   AuthPayload `json:"auth,omitempty"`
+	Action string 		`json:"action"`
+	Auth   AuthPayload  `json:"auth,omitempty"`
+	Log    LogPayload   `json:"log,omitempty"`
 }
 
 type AuthPayload struct{
 	Email string `json:"email"`
 	Password string `json:"password"`
+}
+
+type LogPayload struct{ 
+	Name string `json:"name"`
+	Data string `json:"data"`
 }
 
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request){
@@ -58,13 +64,49 @@ func (app *Config) HandleSubmission(w http.ResponseWriter, r *http.Request){
 	switch requestPayload.Action {
 	case "auth":
 		app.Authenticate(w, requestPayload.Auth)
+	case "log":
+		app.logItem(w, requestPayload.Log)
 	default:
 		app.errorJSON(w, errors.New("invalid action"))
 		return
 	}
 }
 
-func (app *Config) Authenticate(w http.ResponseWriter, a AuthPayload){
+
+func (app *Config) logItem(w http.ResponseWriter, entry LogPayload){
+	jsonData, _ := json.MarshalIndent(entry, "", "\t")
+
+	logServiceURL := "http://logger-service/log"
+	
+	request, err := http.NewRequest("POST", logServiceURL, bytes.NewBuffer(jsonData))
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	request.Header.Set("Content-Type", "application/json")
+	
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+	defer response.Body.Close()
+	if response.StatusCode != http.StatusAccepted {
+		app.errorJSON(w, errors.New("error calling logger service"))
+		return
+	}
+
+	var payload jsonResponse
+	payload.Error = false
+	payload.Message = "logged!"
+
+	app.writeJSON(w, http.StatusOK, payload)
+}
+
+
+func (app *Config) authenticate(w http.ResponseWriter, a AuthPayload){
 	// create some json we'll send to the auth microservice
 	jsonData, _ := json.MarshalIndent(a, "","\t")
 	// call the service
