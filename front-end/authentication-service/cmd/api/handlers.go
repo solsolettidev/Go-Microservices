cb8a4f3b397e0c276c875b3ba40faf562f904eb6
Sol Soletti <solettisool@gmail.com>
2026-04-20 20:37:22 -0300

    add logging to the authentication service

--- front-end/authentication-service/cmd/api/handlers.go
+++ front-end/authentication-service/cmd/api/handlers.go
@@ -31,6 +31,14 @@ func(app *Config) Authenticate(w http.ResponseWriter, r *http.Request){
 		return
 	}
 
+	// log authentication
+
+	err = app.logRequest(w, "authentication", fmt.Sprintf("User %s logged in", user.Email)) // this is how we log requests to the logger service
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
 	payload := jsonResponse{
 		Error: false,
 		Message: fmt.Sprintf("Logged in user %s", user.Email),
@@ -38,4 +46,37 @@ func(app *Config) Authenticate(w http.ResponseWriter, r *http.Request){
 	}
 
 	app.writeJSON(w, http.StatusOK, payload)
+}
+
+func (app *Config) logRequest(name, data string) error { // this function is used to log requests to the logger service
+	var entry struct{
+		Name string `json:"name"`
+		Data string `json:"data"`
+	}
+
+	entry.Name = name
+	entry.Data = data
+
+	jsonData, _ := json.MarshalIndent(entry, "", "\t")
+
+	logServiceURL := "http://logger-service/log"
+
+	request, err := http.NewRequest("POST", logServiceURL, bytes.NewBuffer(jsonData)) // this is how we send data to the logger service, bytes.NewBuffer is used to convert the json data to a byte buffer
+	if err != nil {
+		return err
+	}
+
+	request.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+	if response.StatusCode != http.StatusAccepted {
+		return errors.New("error calling logger service")
+	}
+
+	return nil
 }
