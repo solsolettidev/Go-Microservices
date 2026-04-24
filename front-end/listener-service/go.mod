2de66057b99f6393a8dc01e50009961f6c6950b5
Sol Soletti <solettisool@gmail.com>
2026-04-23 21:19:03 -0300

    init listener service

---
+++ front-end/listener-service/go.mod
@@ -0,0 +1,5 @@
+module listener
+
+go 1.26.1
+
+require github.com/rabbitmq/amqp091-go v1.11.0 // indirect
---
+++ front-end/listener-service/main.go
@@ -0,0 +1,17 @@
+package main
+
+imoprt(
+
+)
+
+func main() {
+	// try to connect to rabbitmq.
+
+	// start listening for messages, te queue will push messages to us.
+
+	// create consumer, for consuming messages from the queue.
+
+	// watch the queue and consume events from a topic.
+
+
+}
